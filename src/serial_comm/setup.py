a2665dae86b4aefac39cc79ca670bb1834b4a9e6
Hyeongjin_Kim <ky942400@gmail.com>
2025-11-12 19:20:02 +0900

    chk

--- src/serial_comm/setup.py
+++ src/serial_comm/setup.py
@@ -11,6 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['launch/serial_test.launch.py']),
+        ('share/' + package_name, ['launch/teleop_launch.launch.py']),
 
     ],
     install_requires=['setuptools'],
