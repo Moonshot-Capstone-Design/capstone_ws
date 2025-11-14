0648d824fb38ed9e502cd92b1da8a3579e2220d8
Hyeongjin_Kim <ky942400@gmail.com>
2025-11-14 17:18:01 +0900

    tf continuos test

--- src/serial_comm/launch/serial_test.launch.py
+++ src/serial_comm/launch/serial_test.launch.py
@@ -1,42 +1,43 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    node = Node(
+    # 1) 실제 로봇 하드웨어 구동 노드 (모터 + odom + joint_states)
+    base_main_node = Node(
         package="serial_comm",
         executable="base_main_node",
         name="base_main_node",
         output="screen"
     )
+    ld.add_action(base_main_node)
 
-    ld.add_action(node)
-    node_joy = Node(
+    # 2) 조이스틱 노드
+    joy_node = Node(
         package="joy",
         executable="joy_node",
         name="joy_node",
         output="screen"
     )
-    ld.add_action(node_joy)
+    ld.add_action(joy_node)
 
-
-    # Path to the URDF file
-    pkg_path = os.path.join(get_package_share_directory('robot_description'))
+    # 3) URDF 로딩 후 robot_state_publisher
+    pkg_path = get_package_share_directory('robot_description')
     urdf_file = os.path.join(pkg_path, 'description', 'robot_description.urdf')
+
     with open(urdf_file, 'r') as infp:
         robot_description = infp.read()
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description}
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{'robot_description': robot_description}]
     )
     ld.add_action(node_robot_state_publisher)
 
