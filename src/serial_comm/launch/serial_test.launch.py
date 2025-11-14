fbbdf89dd5742bfaac73a67bf240ff3e78b60030
Hyeongjin_Kim <ky942400@gmail.com>
2025-11-14 14:58:44 +0900

    little revise... ing..

---
+++ src/serial_comm/launch/serial_test.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    node = Node(
+        package="serial_test",
+        executable="test_node",
+        name="test_node",
+        output="screen"
+    )
+    ld.add_action(node)
+
+    node_joy = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy_node",
+        output="screen"
+    )
+    ld.add_action(node_joy)
+
+
+    # Path to the URDF file
+    pkg_path = os.path.join(get_package_share_directory('amr_demo'))
+    pkg_path2 = os.path.join(get_package_share_directory('serial_test'))
+    urdf_file = os.path.join(pkg_path, 'description', 'amr_demo_center.urdf')
+    with open(urdf_file, 'r') as infp:
+        robot_description = infp.read()
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+    ld.add_action(node_robot_state_publisher)
+
+    return ld
--- src/serial_comm/launch/teleop_launch.launch.py
+++ src/serial_comm/launch/teleop_launch.launch.py
@@ -10,8 +10,8 @@ def generate_launch_description():
             parameters=[{'dev': '/dev/input/js0'}]
         ),
         Node(
-            package='serial_comm',        # 너의 패키지명으로 변경
-            executable='md_teleop_node',      # 노드 파일 이름
+            package='serial_comm',
+            executable='md_teleop_node',
             name='md_teleop_node',
             output='screen',
             parameters=[{'max_speed': 40}]
