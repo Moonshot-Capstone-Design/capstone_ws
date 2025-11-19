f4de311906c89a880a6735815a1810898f2c56c9
Hyeongjin_Kim <ky942400@gmail.com>
2025-11-19 12:44:10 +0900

    OS.PATH update setup.py

--- src/robot_navigator/setup.py
+++ src/robot_navigator/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'robot_navigator'
 
@@ -7,18 +9,37 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # Package index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
+        # package.xml
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/nav2_bringup/localization_launch.py']),
-        ('share/' + package_name, ['launch/nav2_bringup/navigation_launch.py']),
+
+        # launch files 전체 설치
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch', 'nav2_bringup'),
+            glob('launch/nav2_bringup/*.py')),
+
+        # params 폴더 전체 설치
+        (os.path.join('share', package_name, 'params'),
+            glob('params/*.yaml')),
+
+        # map 폴더 전체 설치
+        (os.path.join('share', package_name, 'map'),
+            glob('map/*')),
+
+        # rviz 폴더 전체 설치
+        (os.path.join('share', package_name, 'rviz'),
+            glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='moonshot',
     maintainer_email='ky942400@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Navigation package for AMR',
+    license='Apache License 2.0',
     extras_require={
         'test': [
             'pytest',
